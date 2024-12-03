bbfb42c6d8b3685c4ac61909ea1acea18c180698
amansubudhi <amansubudhi12@gmail.com>
2024-12-04 00:40:53 +0530

    transfer added

--- apps/bank_webhook/src/index.ts
+++ apps/bank_webhook/src/index.ts
@@ -2,37 +2,47 @@ import express from "express"
 import db from "@repo/db/client"
 
 const app = express();
+app.use(express.json())
 
-app.post("/", async (req, res) => {
+app.post("/hdfcWebhook", async (req, res) => {
     // Add zod validation
     // Check if this request actually came from hdfc bank, use a webhook secret here
     const paymentInformation = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
-    }
-    // Update balance in db, add txn
-    await db.balance.update({
-        where: {
-            userId: paymentInformation.userId
-        },
-        data: {
-            amount: {
-                increment: paymentInformation.amount
-            }
-        }
-    });
+    };
 
-    await db.onRampTransaction.update({
-        where: {
-            token: paymentInformation.token,
-        },
-        data: {
-            status: "Success"
-        }
-    })
-    res.status(200).json({
-        message: "captured"
-    })
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                data: {
+                    amount: {
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token,
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ]);
+        res.status(200).json({
+            message: "captured"
+        })
+    } catch (e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
 })
 
+app.listen(3003);
